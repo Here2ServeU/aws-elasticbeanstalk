109a9d1bc61b60ac08dc70530c11ba6fb545d767
LearnDevOps <revnaweji@gmail.com>
2024-06-07 18:21:21 -0500

    Update server.js

--- JSON/server.js
+++ JSON/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const http = require('node:http');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello T2S geniuses!\n');
 });
 
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
